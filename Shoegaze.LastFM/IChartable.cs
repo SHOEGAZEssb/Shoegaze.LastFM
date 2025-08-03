9aef3cc8d8de8fb204558fb132f5c329b9e73a8d
Tim Stadler <timstadler905@googlemail.com>
2025-08-03 21:09:43 +0200

    Update IChartable.cs

--- Shoegaze.LastFM/IChartable.cs
+++ Shoegaze.LastFM/IChartable.cs
@@ -1,5 +1,9 @@
 ﻿namespace Shoegaze.LastFM
 {
+  /// <summary>
+  /// Describes a type that can be used with
+  /// <see cref="User.IUserApi.GetWeeklyChartAsync{T}(string, DateTime?, DateTime?, CancellationToken)"/>.
+  /// </summary>
   public interface IChartable
   {
   }
